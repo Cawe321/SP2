53b452b9dab1ab5d5a8aa4b7584804d2138b3266
Ryainer <Jonathanjiale@gmail.com>
2020-02-19 14:59:58 +0800

    Update Bouncertask.cpp

--- DM2122 Prac/Source/Bouncertask.cpp
+++ DM2122 Prac/Source/Bouncertask.cpp
@@ -5,7 +5,7 @@ std::string Bouncertask::Taskstatus(std::vector<Vocation::Quest> data)
 	std::string output = "";
 	std::string edit = "";
 	if (data[2].currentNumber < data[2].maxNumber) {
-		output = "Bouncer: Suspicious People to kick out ";
+		output = "Bouncer: Suspects to kick out "; //sorry had to change to suspects or else it wont fit the screen
 		edit = std::to_string(data[2].currentNumber);
 		output.append(edit);
 		output.append("/");

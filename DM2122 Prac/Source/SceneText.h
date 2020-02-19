d9350243599ebf52a1abe490ebb429f57ae83a9d
Ryainer <Jonathanjiale@gmail.com>
2020-02-19 20:59:03 +0800

    Typos at RenderObjectOnScreen
    oops

--- DM2122 Prac/Source/SceneText.h
+++ DM2122 Prac/Source/SceneText.h
@@ -92,7 +92,7 @@ private:
 
 	void RenderText(Mesh* mesh, std::string text, Color color);
 	void RenderTextOnScreen(Mesh* mesh, std::string text, Color color, float size, float x, float y);
-	void RenderObjectOnScreen(Mesh mesh, float size, float x, float y);
+	void RenderObjectOnScreen(Mesh* mesh, float size, float x, float y);
 	void CalculateFrameRate();
 
 public:

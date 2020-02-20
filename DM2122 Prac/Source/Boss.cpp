62027c65a5b8be84cec87e20eb5824dbfeb61822
JTZH <jordantanzh@gmail.com>
2020-02-20 15:30:11 +0800

    Added Boss goodwill aids player when doing jobs well penalizes player if they fail to many times

---
+++ DM2122 Prac/Source/Boss.cpp
@@ -0,0 +1,64 @@
+#include "Boss.h"
+
+Boss::Boss()
+{
+	Goodwill = 50;
+}
+
+Boss::~Boss()
+{
+}
+
+void Boss::AddGoodwill(int input)
+{
+	//input is a rough measure of how well a previous task was done
+	Goodwill = Goodwill + input;
+	if (Goodwill >= 100) {
+		Goodwill = 100;
+	}
+}
+
+void Boss::LoseGoodwill(int input)
+{
+	//input is a rough measure of how poorly a previous task was done
+	Goodwill = Goodwill - input;
+}
+
+int Boss::GetGoodwill()
+{
+	return Goodwill;
+}
+
+float Boss::getmodifier()
+{
+	int ModifiableGoodwill;
+	float ModifiableValue = 0;
+	ModifiableGoodwill = Goodwill - 50;
+	if (ModifiableGoodwill > 0) {
+		if (ModifiableGoodwill < 10) {
+			return 1;
+		}
+		else {
+			while (ModifiableGoodwill > 9) {
+				ModifiableValue++;
+				ModifiableGoodwill = ModifiableGoodwill - 10;
+			}
+		}
+	}
+	else {
+		ModifiableGoodwill = ModifiableGoodwill * (-1);
+		if (ModifiableGoodwill < 10) {
+			return 1;
+		}
+		else {
+			while (ModifiableGoodwill > 9) {
+				ModifiableValue++;
+				ModifiableGoodwill = ModifiableGoodwill - 10;
+			}
+		}
+		ModifiableValue = ModifiableValue * (-1);
+	}
+	ModifiableValue = ModifiableValue / 10;
+	ModifiableValue = ModifiableValue + 1;
+	return ModifiableValue;
+}

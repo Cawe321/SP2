ddf45cadd0f5596c685ee8d7eccd235b5d325b28
Ryainer <Jonathanjiale@gmail.com>
2020-02-22 14:32:43 +0800

    made RenderCommandTextBox take in string

--- DM2122 Prac/Source/SceneText.h
+++ DM2122 Prac/Source/SceneText.h
@@ -177,7 +177,7 @@ private:
 	void Renderlevel();
 	
 	void RenderBouncerTextBox();
-	void RenderCommandTextBox();
+	void RenderCommandTextBox(std::string text);
 
 	void RenderText(Mesh* mesh, std::string text, Color color);
 	void RenderTextOnScreen(Mesh* mesh, std::string text, Color color, float size, float x, float y);

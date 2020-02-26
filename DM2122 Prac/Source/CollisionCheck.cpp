f2aebb1eaccc71f067b812e2da95b7cd8f0ae4a2
Cawe321 <calebauw123@gmail.com>
2020-02-26 22:42:29 +0800

    Add CollisionCheck class

---
+++ DM2122 Prac/Source/CollisionCheck.cpp
@@ -0,0 +1,32 @@
+#include "CollisionCheck.h"
+
+
+
+int CollisionCheck::DistanceCheck(Vector3 obj1, Vector3 obj2)
+{
+	return sqrt(pow(obj1.x - obj2.x, 2) + pow(obj1.y - obj2.y, 2) + pow(obj1.z - obj2.z, 2));
+}
+
+bool CollisionCheck::SquareCollisionCheck(Vector3 obj1, float obj1SizeX, float obj1SizeZ, Vector3 obj2, float obj2SizeX, float obj2SizeZ)
+{
+	bool collisionX = obj1.x + obj1SizeX >= obj2.x && obj2.x + obj2SizeX >= obj1.x;
+	// Collision y-axis?
+	bool collisionZ = obj1.z + obj1SizeZ >= obj2.z && obj2.z + obj2SizeZ >= obj1.z;
+	// Collision only if on both axes
+	return collisionX && collisionZ;
+}
+
+bool CollisionCheck::TrueCollisionCheck(Vector3 obj1, Vector3 obj2)
+{
+	// not done yet
+	return false;
+}
+
+CollisionCheck::CollisionCheck()
+{
+}
+
+
+CollisionCheck::~CollisionCheck()
+{
+}
---
+++ DM2122 Prac/Source/CollisionCheck.h
@@ -0,0 +1,15 @@
+#ifndef COLLISIONCHECK_H
+#define COLLISIONCHECK_H
+#include "Vector3.h"
+#include <cmath>
+class CollisionCheck
+{
+public:
+	static int DistanceCheck(Vector3 obj1, Vector3 obj2);
+	static bool SquareCollisionCheck(Vector3 obj1, float obj1SizeX, float obj1SizeZ, Vector3 obj2, float obj2SizeX, float obj2SizeZ);
+	static bool TrueCollisionCheck(Vector3 obj1, Vector3 obj2); // this is the actual collision check for the map
+	CollisionCheck();
+	~CollisionCheck();
+};
+
+#endif

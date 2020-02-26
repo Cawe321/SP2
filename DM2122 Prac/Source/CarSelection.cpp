0d6d31935332f86b8929f5fe32c01a167c732cd1
192352U <57719258+192352U@users.noreply.github.com>
2020-02-26 11:00:53 +0800

    Add files via upload

---
+++ DM2122 Prac/Source/CarSelection.cpp
@@ -0,0 +1,109 @@
+#include "CarSelection.h"
+#include "Bank.h"
+#include <iostream>
+
+CarSelection::CarSelection()
+{
+	first = last = indicator = nullptr;
+	noOfCars = 12;
+	count = 0;
+}
+
+CarSelection::~CarSelection()
+{
+	//
+}
+
+void CarSelection::Up()
+{
+	if (indicator->GetBack() != nullptr)
+		indicator = indicator->GetBack();
+}
+
+void CarSelection::Down()
+{
+	if (indicator->GetNext() != nullptr)
+		indicator = indicator->GetNext();
+}
+
+void CarSelection::AddCar(std::string name, float price)
+{
+	Car* newNode = new Car();
+	if (count < noOfCars)
+	{
+		if ((first == NULL) && (last == NULL))
+		{
+			last = new Car();
+			last->SetValues(name, price);
+			first = last;
+		}
+		else
+		{
+			newNode->SetValues(name, price);
+			newNode->SetBack(last);
+			last->SetNext(newNode);
+			last = newNode;
+		}
+		++count;
+	}
+	indicator = first;
+}
+
+Car* CarSelection::GetIndicator()
+{
+	return indicator;
+}
+
+void CarSelection::Printing()
+{
+	Car* temp = first;
+	while (temp != nullptr)
+	{
+		std::cout << temp->GetName() << "										$" << temp->GetPrice() << "\n";
+		temp = temp->GetNext();
+		if (temp == nullptr)
+			break;
+	}
+}
+
+std::string CarSelection::PrintName(int position)
+{
+	Car* temp = first;
+
+	int localposition = 0;
+	while (temp != nullptr)
+	{
+		if (localposition == position) {
+			return temp->GetName();
+		}
+		else {
+			localposition++;
+			temp = temp->GetNext();
+		}
+	}
+}
+
+std::string CarSelection::PrintPrice(int position)
+{
+	Car* temp = first;
+
+	int localposition = 0;
+	while (temp != nullptr)
+	{
+		if (localposition == position)
+			return std::to_string(temp->GetPrice());
+		else 
+		{
+			localposition++;
+			temp = temp->GetNext();
+		}
+	}
+}
+
+std::string CarSelection::PrintSelection(CarSelection* Sel)
+{
+	Car* Temp;
+	Temp = Sel->GetIndicator();
+	return Temp->GetName();
+}
+
---
+++ DM2122 Prac/Source/CarSelection.h
@@ -0,0 +1,25 @@
+#ifndef CARSELECTION_H
+#define CARSELECTION_H
+#include "Car.h"
+
+class CarSelection
+{
+private:
+	Car* first, * last, * indicator;
+	int noOfCars;
+	int count;
+
+public:
+	CarSelection();
+	~CarSelection();
+
+	void Up();
+	void Down();
+	void AddCar(std::string name, float price);
+	Car* GetIndicator();
+	void Printing();
+	std::string PrintName(int);
+	std::string PrintPrice(int);
+	std::string PrintSelection(CarSelection*);
+};
+#endif
